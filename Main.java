5c610b8703c02d9726f873ba857f661ce2837297
Artem Prytkov <aprytkov@gmail.com>
2020-08-20 22:17:24 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.mult("1011","0110"));
+        System.out.println(bins.sum("1011","0110"));
+    }
+}
